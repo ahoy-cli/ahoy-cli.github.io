c2881fba8a8090f3cf968597c40719a3e8e3d177
Drew Robinson <drew.robinson@gmail.com>
2025-08-19 12:20:06 +1000

    Chore: fix Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,53 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightThemeRapide from 'starlight-theme-rapide';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'Ahoy',
-      description: 'Create self-documenting CLI programs with ease, using YAML files in your source tree',
-      customCss: [
-        './src/styles/custom.css',
-      ],
-      // plugins: [
-      //   starlightThemeRapide(),
-      // ],
-      social: {
-        github: 'https://github.com/ahoy-cli/ahoy',
+      title: "Ahoy",
+      description:
+        "Create self-documenting CLI programs with ease, using YAML files in your source tree",
+      logo: {
+        src: "./src/assets/logo.svg",
       },
+      customCss: ["./src/styles/custom.css"],
+
+      social: [
+        {
+          icon: "github",
+          href: "https://github.com/ahoy-cli/ahoy",
+          label: "GitHub",
+        },
+      ],
       sidebar: [
         {
-          label: 'Getting Started',
+          label: "Getting Started",
           items: [
-            { label: 'Installation & Setup', slug: 'guides/getting-started' },
-            { label: 'Shell Autocompletion', slug: 'guides/shell-autocompletion' },
+            { label: "Installation & Setup", slug: "guides/getting-started" },
+            { label: "Next Steps", slug: "guides/next-steps" },
+            {
+              label: "Shell Autocompletion",
+              slug: "guides/shell-autocompletion",
+            },
           ],
         },
         {
-          label: 'Guides',
+          label: "Guides",
           items: [
-            { label: 'YAML Configuration', slug: 'guides/yaml-config' },
-            { label: 'Command Execution', slug: 'guides/command-execution' },
-            { label: 'Importing & Overriding', slug: 'guides/importing' },
-            { label: 'Environment', slug: 'guides/environment' },
+            { label: "YAML Configuration", slug: "guides/yaml-config" },
+            { label: "Command Execution", slug: "guides/command-execution" },
+            { label: "Importing & Overriding", slug: "guides/importing" },
+            { label: "Environment", slug: "guides/environment" },
           ],
         },
         {
-          label: 'Reference',
+          label: "Reference",
           items: [
-            { label: 'CLI Reference', slug: 'reference/cli' },
-            { label: 'Environment Reference', slug: 'reference/environment' },
-            { label: 'YAML Schema', slug: 'reference/yaml-schema' },
-          ],
-        },
-        {
-          label: 'Top',
-          items: [
-            { label: 'Installation', slug: 'installation' },
-            { label: 'Introduction', slug: 'introduction' },
-            { label: 'Quick Start', slug: 'quick-start' },
+            { label: "CLI Reference", slug: "reference/cli" },
+            { label: "Environment Reference", slug: "reference/environment" },
+            { label: "YAML Schema", slug: "reference/yaml-schema" },
           ],
         },
       ],
